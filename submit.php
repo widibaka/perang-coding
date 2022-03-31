ef8e2406d8a61c26f7aede4e00762752603832a9
widibaka <widi_dwi@fikom.udb.ac.id>
2022-03-31 23:16:12 +0700

    Bagus

--- submit.php
+++ submit.php
@@ -1,9 +1,19 @@
 <?php 
 
-function run()
-{
-  return eval($_POST['code']);
+try {
+  $data['hasil'] = eval($_POST['code']);
+  $data['soal_berlanjut'] = (
+    file_exists( 'soal'.$_POST['nomor_soal'].'/soal.php' )
+    AND file_exists( 'soal'.$_POST['nomor_soal'].'/jawaban.php')
+    AND file_exists( 'soal'.$_POST['nomor_soal'].'/instruksi.php' )
+  );
+  if ( $data['hasil'] ) {
+    echo json_encode($data);
+  }
+}
+//catch exception
+catch(Exception $e) {
+  echo $e->getMessage();
+  die();
 }
 
-
-echo run(); die;
